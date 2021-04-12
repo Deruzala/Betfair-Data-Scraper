f384e911c4884e5683b8b93a263f0c051a17ef3b
Deruzala <55004185+Deruzala@users.noreply.github.com>
2021-04-12 11:16:10 +0100

    Returns & Liabilities

--- Returns.sql
+++ Returns.sql
@@ -7,6 +7,6 @@ SELECT
 
 	CASE WHEN WIN_LOSE = 1 THEN (BSP - 1.0) ELSE -1 END AS Profit,
 	FORMAT(1.0 * (BSP-1.0),'0.00') AS LayLiability,
-    FORMAT(IIF(WIN_LOSE = 1, (1.0 * (BSP-1.0) * -1), 1),'0.00') AS LayReturn
+    	FORMAT(IIF(WIN_LOSE = 1, (1.0 * (BSP-1.0) * -1), 1),'0.00') AS LayReturn
 
 FROM PlaceData
