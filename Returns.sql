96e5bf04c4735aa66d0be3e4af9a0177c647ed1d
Deruzala <55004185+Deruzala@users.noreply.github.com>
2021-04-12 11:11:18 +0100

    Returns & Liabilities
    + Calculates Back Return to BSP
    + Lay Liability
    + Lay Return

---
+++ Returns.sql
@@ -0,0 +1,12 @@
+CREATE VIEW [dbo.Returns]
+AS
+
+SELECT
+	SELECTION_NAME
+	BSP,
+
+	CASE WHEN WIN_LOSE = 1 THEN (BSP - 1.0) ELSE -1 END AS Profit,
+	FORMAT(1.0 * (BSP-1.0),'0.00') AS LayLiability,
+    FORMAT(IIF(WIN_LOSE = 1, (1.0 * (BSP-1.0) * -1), 1),'0.00') AS LayReturn
+
+FROM PlaceData
