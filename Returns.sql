ec0f0d2fd77b9e5c7ce1cccbcacbcb2b625f8709
Deruzala <55004185+Deruzala@users.noreply.github.com>
2021-05-18 12:51:08 +0100

    Returns.sql
    Described conditional statements with examples
    
    + Case Expression
    + IIF Function

--- Returns.sql
+++ Returns.sql
@@ -4,8 +4,19 @@ AS
 SELECT
 	SELECTION_NAME
 	BSP,
-	CASE WHEN WIN_LOSE = 1 THEN (BSP - 1.0) ELSE -1 END AS Profit,
+	
+	-- A 'case expression' can be used for a conditional statement
+	(CASE WHEN WIN_LOSE = 1 THEN (BSP - 1.0) ELSE -1.0 END) AS Return,
+	(CASE WHEN place = 1 THEN (BSP - 1.0) * .02 ELSE 0 END) AS backCommission,
+	(CASE WHEN place = 1 THEN (BSP - 1.0) - ((BFSP - 1.0) * .02) ELSE -1.0 END) AS netReturn,
+	
+	-- The 'format' function restricts decimal places
 	FORMAT(1.0 * (BSP-1.0),'0.00') AS LayLiability,
-    	FORMAT(IIF(WIN_LOSE = 1, (1.0 * (BSP-1.0) * -1), 1),'0.00') AS LayReturn
+	
+	-- An 'IIF' function can also be used for conditional statements
+    	IIF(WIN_LOSE = 1, (1.0 * (BFSP-1.0) * -1.0), 1.0) AS layReturn,
+	IIF(WIN_LOSE = 1, 0, 0.20 END) AS layCommission,
+	IIF(WIN_LOSE = 1, (1.0 * (BFSP-1.0) * -1), 0.98) AS netLayReturn
+	
 
 FROM PlaceData
