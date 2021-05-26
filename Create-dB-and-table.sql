76d06d89ba6ef5834ea170df5e8d36e69b7e7e52
Deruzala <55004185+Deruzala@users.noreply.github.com>
2021-05-26 02:48:29 +0100

    + WinData table

--- Create-dB-and-table.sql
+++ Create-dB-and-table.sql
@@ -4,6 +4,29 @@ GO
 USE BetfairData
 GO
 
+CREATE TABLE WinData
+
+(
+  EVENT_ID bigint,
+  MENU_HINT varchar(500),
+  EVENT_NAME varchar(500),
+  EVENT_DT datetime2(0),
+  SELECTION_ID bigint,
+  SELECTION_NAME varchar(500),
+  WIN_LOSE bigint,
+  BSP float,
+  PPWAP float,
+  MORNINGWAP float,
+  PPMAX float,
+  PPMIN float,
+  IPMAX float,
+  IPMIN float,
+  MORNINGTRADEDVOL float,
+  PPTRADEDVOL float,
+  IPTRADEDVOL float,
+);
+GO
+
 CREATE TABLE PlaceData
 
 (
